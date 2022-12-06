0cf1dd27af7690c1a9682731377211f177f34436
Razmaster666 <113451916+Razmaster666@users.noreply.github.com>
2022-12-06 11:16:06 +0100

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,23 @@
+let drDoom = 0;
+let magneto = 0;
+
+// const yesBtn = document.getElementById('yes');
+// yesBtn.checked = true;
+
+// function addMagneto() {
+//     magneto++
+//     console.log("Magneto")
+//     console.log(magneto)
+// }
+
+// function addDrDoom() {
+//     drDoom++
+//     console.log("Doom")
+//     console.log(drDoom)
+// }
+
+function goToNextQ() {
+    const answer = document.querySelectorAll("input:checked")[0].id
+
+    console.log(answer)
+}
